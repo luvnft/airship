c79d99e274bd1a725cb984e918e2f4908bfbe617
The Wizard of Hahz <info@luvnft.com>
2024-11-28 08:51:18 -0500

    up

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="flex justify-center w-full space-x-4">
       <a
-        href="https://twitter.com/hahzlove"
+        href="https://twitter.com/wizardofhahz"
         target="_blank"
         rel="noopener noreferrer"
         className="text-white hover:text-primary/90"
